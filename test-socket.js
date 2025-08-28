5c7eb6330d7b5cd51c01b70a449e58ba0cad531f
Vansh Maurya <72353067+va24nsh@users.noreply.github.com>
2025-08-28 13:33:30 +0530

    Update socket client to use new message structure

--- test-socket.js
+++ test-socket.js
@@ -2,7 +2,7 @@ const { io } = require("socket.io-client");
 
 // Replace with your actual JWT token
 const token =
-  "eyJpZCI6MSwiaWF0IjoxNzU1NzU4ODU1fQ.R2xlIXGm9JmORYRVVEhYjxSGGvJAS4TZSt1P06Z1DY4";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzU1NzU4ODU1fQ.R2xlIXGm9JmORYRVVEhYjxSGGvJAS4TZSt1P06Z1DY4";
 
 const socket = io("http://localhost:4000", {
   path: "/socket.io",
@@ -12,27 +12,32 @@ const socket = io("http://localhost:4000", {
 socket.on("connect", () => {
   console.log("Connected:", socket.id);
 
-  // List messages
-  socket.emit("LIST_MESSAGES", { conversationId: "abc123" }, (res) => {
-    console.log("LIST_MESSAGES response:", res);
-  });
+  // List conversation messages
+  socket.emit(
+    "LIST_MESSAGES",
+    { conversationId: "68b002eaaf68103a2fd91c30" },
+    (res) => {
+      console.log("LIST_CONVERSATION_MESSAGES response:", res);
+    }
+  );
 
-  // Send a message
+  // Send a message to a conversation
   socket.emit(
     "SEND_MESSAGE",
     {
       conversationId: "abc123",
       toUserId: "user2",
-      content: "Hello!",
+      content: "Hello from socket client!",
     },
     (res) => {
-      console.log("SEND_MESSAGE response:", res);
+      console.log("SEND_CONVERSATION_MESSAGE response:", res);
     }
   );
 });
 
-socket.on("NEW_MESSAGE", (payload) => {
-  console.log("Received NEW_MESSAGE:", payload);
+// Listen for new incoming messages in a conversation
+socket.on("NEW_CONVERSATION_MESSAGE", (payload) => {
+  console.log("Received NEW_CONVERSATION_MESSAGE:", payload);
 });
 
 socket.on("disconnect", (reason) => {
